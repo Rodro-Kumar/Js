adf3be9e2d1935ab0ef7ab5056a64ea8221bc88f
Rodro-Kumar <rudrokormokar96@gmail.com>
2023-12-08 12:14:19 +0600

    Basic Practice

---
+++ app.js
@@ -0,0 +1,360 @@
+// let x = 2;
+// let y = 4;
+
+// let sum = x + y;
+
+// console.log(sum);
+
+// let x = 34;
+// x = 20;
+// let y = 30;
+// y = 20;
+
+// let jog = x + y;
+
+// console.log(jog);
+
+// var x = 6;
+// var x = 10;
+// var x = 23;
+// var x = 20;
+// var y = 23;
+
+// var jog = y;
+
+// console.log(jog);
+
+// const x = 3.25454;
+// // x = 2.2145454; <=== overwrite kora jabo nah
+
+// console.log(x);
+
+// let x = "20";
+// let y = 20;
+
+// let sum = x === y;
+
+// console.log(sum);
+
+// let x = 13;
+// x = x + 10;
+// x = x - 10;
+
+// console.log(x);
+
+// var x = 12;
+// var y = 20;
+
+// let modulus = x * y;
+
+// console.log(modulus);
+
+// let hello = 20;
+// hello = 30;
+// hello = 40;
+
+// let x = true;
+// let y = true;
+
+// let and = x && y;
+
+// console.log(and);
+
+// let x = true;
+// let y = false;
+
+// let or = x || y;
+
+// console.log(or);
+
+// let x = true;
+// let y = true;
+
+// var and = x && y;
+
+// let x = false;
+
+// var x = 23;
+// var y = 20;
+
+// var sum = x + y;
+
+// alert(sum);
+
+// let x = hello;
+
+// var x = 3;
+
+// var y = 2;
+
+// var sum = x + y;
+
+// alert(sum);
+
+// let num1 = prompt("Enter Your First value");
+// let num2 = prompt("Enter Your Second value");
+
+// if (num1 > num2) {
+//   // 10 > 15
+//   console.log("Greter Number is", num2);
+// } else if (num1 < num2) {
+//   // 10 < 15
+//   console.log("gretter number is", num2);
+// } else {
+//   console.log("both");
+// }
+
+// let number1 = prompt("Enter your first value");
+// let number2 = prompt("Enter your second value");
+// //    20 > 10;
+// if (number1 > number2) {
+//   console.log("gretter number is ", number1);
+//   // 20 < 10
+// } else if (number1 < number2) {
+//   console.log("small number", number1);
+// } else {
+//   console.log("Value same");
+// }
+
+// todo : Conditional statement
+
+// let marks = 32;
+
+// if (marks == 80) {
+//   console.log("You achieve A+");
+// } else if (marks == 70) {
+//   console.log("Grad A");
+// } else if (marks == 60) {
+//   console.log("Grad A-");
+// } else if (marks == 50) {
+//   console.log("Grad B");
+// } else if (marks == 40) {
+//   console.log("Grad C");
+// } else if (marks == 33) {
+//   console.log("Grad D");
+// } else {
+//   console.log("You fail");
+// }
+
+// let marks = prompt("Enter Your Mark");
+
+// if (marks > 100) {
+//   console.log("Wrong 0 to 100");
+// } else if (marks >= 90 && marks <= 100) {
+//   console.log("You got Golden A+");
+// } else if (marks >= 80 && marks <= 89) {
+//   console.log("You Got A+");
+// } else if (marks >= 70 && marks <= 79) {
+//   console.log("You got A");
+// } else if (marks >= 60 && marks <= 69) {
+//   console.log("You got A-");
+// } else if (marks >= 50 && marks <= 59) {
+//   console.log("You got B");
+// } else if (marks >= 40 && marks <= 49) {
+//   console.log("You got C");
+// } else if (marks >= 33 && marks <= 39) {
+//   console.log("You got D");
+// } else if (marks >= 0 && marks <= 32) {
+//   console.log("You Got Fail");
+// } else if (marks < 0) {
+//   console.log("Negative Number");
+// } else {
+//   console.log("Invalid");
+// }
+
+// let age = prompt("Enter Your Age");
+
+// if (age > 120) {
+//   console.log("Not Allow");
+// } else if (age >= 18 && age <= 120) {
+//   console.log("You can give Your Vote");
+// } else if (age < 0) {
+//   console.log("Invalid");
+// } else {
+//   console.log("Tumi vote dite parba nah");
+// }
+
+// let num = prompt("Enter Your Num");
+
+// if (num % 2 == 0) {
+//   console.log("Your Num is even");
+// } else {
+//   console.log("Your Num is Odd");
+// }
+
+// let number = prompt("Enter Your First Value");
+// let number2 = prompt("ENter Your Second Value");
+
+// if (number > number2) {
+//   console.log("The Big Number :" + number);
+// } else {
+//   console.log("The Small Number :" + number);
+// }
+
+// let num1 = 10;
+// let num2 = 12;
+// let num3 = 40;
+
+// if (num1 > num2) {
+//   console.log("grater num is ", num1);
+// } else if (num2 < num3) {
+//   console.log("Gratter number is ", num3);
+// } else {
+//   console.log("gratter num ", num2);
+// }
+
+// todo : Switch
+
+// let day = prompt("Enter The day");
+
+// switch (day) {
+//   case "saturday": {
+//     console.log("Saturday is offday");
+//     break;
+//   }
+//   case "sunday": {
+//     console.log("sunday is not offday");
+//     break;
+//   }
+//   case "monday": {
+//     console.log("monday is not offday");
+//     break;
+//   }
+//   default: {
+//     console.log("kkichu mile nai");
+//   }
+// }
+
+// let value1 = prompt("Enter Your First Value");
+// let value2 = prompt("Enter Your Second Value");
+// let value3 = prompt("Enter Your Third Value");
+
+// if (value1 > value2) {
+//   console.log("Big number is :", value1);
+// } else if (value2 < value3) {
+//   console.log("Big number is :", value2);
+// }
+
+// let rodro = prompt("Enter The Age");
+// let durjoy = prompt("Enter The Age");
+// let ayan = prompt("Enter The Age");
+
+// switch (rodro) {
+//   case "19": {
+//     console.log("Rodro is 19 years old");
+//     break;
+//   }
+//   default: {
+//     console.log("This is not Rodro Age");
+//   }
+// }
+// switch (durjoy) {
+//   case "20": {
+//     console.log("Durjoy is 20 years old");
+//     break;
+//   }
+//   default: {
+//     console.log("This is not Durjoy Age");
+//   }
+// }
+// switch (ayan) {
+//   case "21": {
+//     console.log("Ayan is 21 years old");
+//     break;
+//   }
+//   default: {
+//     console.log("This is not Ayan Age");
+//   }
+// }
+
+// let age = prompt("Enter Your Age");
+
+// switch (age) {
+//   case "19": {
+//     console.log("Rodro is 19 years old");
+//     break;
+//   }
+//   case "21": {
+//     console.log("Durjoy is 21 years old");
+//     break;
+//   }
+//   case "23": {
+//     console.log("Ayan is 23 years old");
+//     break;
+//   }
+
+//   default: {
+//     console.log("Wrong answer");
+//   }
+// }
+
+// let num1 = prompt("Enter the number");
+// let num2 = prompt("Enter the number");
+// let num3 = prompt("Enter the number");
+
+// if (num1 > num2) {
+//   console.log("The Big number is ", num1);
+// } else if (num2 > num1) {
+//   console.log("The Big number is ", num2);
+// } else if (num3 < num2) {
+//   console.log("The Big number is ", num3);
+// } else {
+//   console.log("Same number");
+// }
+
+// let marks = prompt("Enter Your Marks");
+
+// if (marks > 100) {
+//   console.log("Your Marks Is not Valid");
+// } else if (marks >= 90 && marks <= 100) {
+//   console.log("You got Golden A+");
+// } else if (marks >= 80 && marks <= 89) {
+//   console.log("You got A+");
+// } else if (marks >= 70 && marks <= 79) {
+//   console.log("You Got A");
+// } else if (marks >= 60 && marks <= 69) {
+//   console.log("You Got A-");
+// } else if (marks >= 50 && marks <= 59) {
+//   console.log("You Got B");
+// } else if (marks >= 40 && marks <= 49) {
+//   console.log("You Got C");
+// } else if (marks >= 33 && marks <= 39) {
+//   console.log("You Got D");
+// } else {
+//   console.log("Sorry You Fail");
+// }
+
+// let number1 = prompt("Enter The Number");
+// let number2 = prompt("Enter The Number");
+// let number3 = prompt("Enter The Number");
+
+// if (number1 > number2) {
+//   console.log("The Greater Number is :", number1);
+// } else if (number2 > number1) {
+//   console.log("The Greater Number :", number2);
+// } else if (number3 > number1) {
+//   console.log("The Greater Number is :", number3);
+// } else {
+//   console.log("Same");
+// }
+
+// let signal = prompt("Whats The Signal");
+
+// switch (signal) {
+//   case "red": {
+//     console.log("Stop");
+//     break;
+//   }
+//   case "green": {
+//     console.log("Go Away");
+//     break;
+//   }
+//   case "yellow": {
+//     console.log("Attention");
+//     break;
+//   }
+
+//   default: {
+//     console.log("Wrong");
+//   }
+// }
